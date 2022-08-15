ad70558f26a656ff88e2170d9d9bb2b048c32d14
Caitlin O'Reilly <coreilly29@gmail.com>
2022-08-15 13:51:32 -0400

    randomize fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -50,6 +50,14 @@ var fightOrSkip = function () {
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function (enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+
+  //randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+  
   while (playerInfo.health > 0 && enemy.health > 0) {
     // ask player if they'd like to fight or run
     var promptFight = window.prompt(
